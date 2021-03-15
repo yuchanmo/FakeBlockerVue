e6aa81fc7c7ee55813f2f0407ddbe562af87c87f
mojjijji@gmail.com <mojjijji@gmail.com>
2021-03-15 09:43:45 +0900

    initialize

---
+++ test.js
@@ -0,0 +1,53 @@
+// // let a = Array(11)
+// // a[10] = 1
+// // console.log(a)
+// // console.log(typeof(a))
+// // let f = new File()
+// // console.log(typeof(f))
+
+// // // <rect
+// // // v-for="(box, i) in selected_img[tick].boxes"
+// // // :key="i"
+// // // :x="box[0]"
+// // // :y="box[1]"
+// // // :width="box[2]"
+// // // :height="box[3]"
+// // // ></rect>
+
+// // let axios = require('axios')
+// // let FormData =require('form-data')
+// // let fs = require('fs')
+// // let request = require('request')
+// // let f = fs.readFileSync(
+// //   'D:/Programming/clab/Fakeblocker/KakaoTalk_20210127_225601184.jpg'
+// // )
+
+// // console.log(typeof f)
+// // console.log('ee')
+// // //axios.get('http://127.0.0.1:5000/generate').then((v) => console.log(v.data))
+
+// // console.log('heeloo')
+
+// // request.
+// // let payload = new FormData()
+// // payload.append('image', f)
+// // payload.append('threshold', 9)
+// // console.log(payload)
+
+// // // let res = await axios.post('http://127.0.0.1:5000/generate', payload)
+// // // console.log(res)
+
+// // // console.log('hello')
+
+// let a = [[1,2,3,4],[5,6,7,8]]
+// a.map((v,i)=>v.map((vv,ii)=>{
+//   if(ii<2){
+//     return vv*3
+//   }
+//   else{
+//     return vv
+//   }
+// }))
+
+let a = 0.4343535
+console.log(a.toFixed(3) * 100)
