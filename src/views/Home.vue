a43f237b12e17f1576b98d4b9f69bde576d9f25a
mojjijji@gmail.com <mojjijji@gmail.com>
2021-03-15 10:34:52 +0900

    save

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container>
+    <v-row align="center" justify="center">
+      <v-col cols="12">
+        <v-img src="@/assets/main.png"></v-img>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    imgsrc: '../assets/main.png'
+  })
+}
+</script>
+
+<style></style>
